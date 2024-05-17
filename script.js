108e86a4eb0a4a07ad36be89045b558a1ec2d22b
Zach Poba-Nzaou <168301948+plectus24@users.noreply.github.com>
2024-05-16 21:45:19 -0400

    Add basic ui
    Added tracking for player and computer score
    Created a button to restart the game

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let humanScore, computerScore;
+let humanScore = 0,
+  computerScore = 0;
+
+// logic for computer choice
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 100);
   if (random >= 0 && random < 33) {
@@ -13,6 +16,7 @@ function getComputerChoice() {
   }
 }
 
+// logic for player choice
 function getHumanChoice() {
   let humanChoice = prompt('Please select "rock", "paper" or "scissor"');
   if (typeof humanChoice !== "string") {
@@ -31,30 +35,39 @@ function getHumanChoice() {
   }
 }
 
+//Logic for playing a round
 function playRound(humanChoice, computerChoice) {
+  const board = document.querySelector(".board");
+  board.textContent = "";
   if (humanChoice === "rock") {
     if (computerChoice === "rock") {
       console.log("Tie !");
+      board.textContent += "Tie !";
     }
     if (computerChoice === "paper") {
       console.log("Computer wins !");
+      board.textContent += "Computer wins !";
       computerScore++;
     }
     if (computerChoice === "scissor") {
       console.log("You win !");
+      board.textContent += "You win!";
       humanScore++;
     }
   }
   if (humanChoice === "paper") {
     if (computerChoice === "rock") {
       console.log("You win !");
+      board.textContent += "You win!";
       humanScore++;
     }
     if (computerChoice === "paper") {
       console.log("Tie !");
+      board.textContent += "Tie !";
     }
     if (computerChoice === "scissor") {
       console.log("Computer wins !");
+      board.textContent += "Computer wins !";
       computerScore++;
     }
   }
@@ -62,35 +75,90 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice === "scissor") {
     if (computerChoice === "rock") {
       console.log("Computer wins !");
+      board.textContent += "Computer wins !";
       computerScore++;
     }
     if (computerChoice === "paper") {
       console.log("You win !");
+      board.textContent += "You win!";
       humanScore++;
     }
     if (computerChoice === "scissor") {
       console.log("Tie !");
+      board.textContent += "Tie !";
     }
   }
-}
-
-function playGame() {
-  (humanScore = 0), (computerScore = 0);
-  const totalGames = parseInt(prompt("How many games will you play?"));
-
-  let computerSelection = "";
-
-  let humanSelection = "";
-  for (let i = 0; i < totalGames; i++) {
-    humanSelection = getHumanChoice();
-    computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-
   console.log(`
-  Total games played: ${totalGames}
-  Final Player Score: ${humanScore}
-  Final Computer Score: ${computerScore}`);
+  Human Score: ${humanScore}
+  Computer Score: ${computerScore}
+  `);
 }
 
-playGame();
+// logic for displaying game score
+function displayScore() {
+  let score = document.querySelector(".score");
+  //Allow text wrapping for template litteral
+  score.style["white-space"] = "pre-wrap";
+
+  score.textContent = `
+  Human Score: ${humanScore} 
+  Computer Score: ${computerScore}
+  `;
+  if (humanScore === 5) {
+    score.textContent += `
+  Human WIN
+  `;
+  } else if (computerScore === 5) {
+    score.textContent += `
+  COMPUTER WIN
+  `;
+  }
+}
+
+// function playGame() {
+//   (humanScore = 0), (computerScore = 0);
+//   const totalGames = parseInt(prompt("How many games will you play?"));
+
+//   let computerSelection = "";
+
+//   let humanSelection = "";
+//   for (let i = 0; i < totalGames; i++) {
+//     humanSelection = getHumanChoice();
+//     computerSelection = getComputerChoice();
+//     playRound(humanSelection, computerSelection);
+//   }
+
+//   console.log(`
+//   Total games played: ${totalGames}
+//   Final Player Score: ${humanScore}
+//   Final Computer Score: ${computerScore}`);
+// }
+
+function restartGame() {
+  const board = document.querySelector(".board");
+  const restart = document.querySelector(".restart");
+  restart.addEventListener("click", (e) => {
+    console.log("fwg");
+    humanScore = 0;
+    computerScore = 0;
+    board.textContent = "";
+    displayScore();
+  });
+}
+
+let buttons = [...document.querySelectorAll(".button")];
+
+buttons.forEach((item) => {
+  // STOP GAME IF SCORE = 5
+  item.addEventListener("click", (event) => {
+    if (humanScore === 5 || computerScore === 5) {
+      return 0;
+    }
+    playRound(event.target.textContent.toLowerCase(), getComputerChoice());
+    displayScore();
+  });
+});
+//Initial score display
+displayScore();
+// restart the game
+restartGame();
